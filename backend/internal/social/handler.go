b782f8c114049baac6f6b40002ae3bc37c3c6b67
SepTem61r <SepTem61r@example.com>
2026-05-10 19:34:45 +0800

    social handler 逻辑

--- backend/internal/social/handler.go
+++ backend/internal/social/handler.go
@@ -1,1 +1,103 @@
 package social
+
+import (
+	"feedsystem_video/backend/internal/middleware/jwt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type SocialHandler struct {
+	socialService *SocialService
+}
+
+func NewSocialHandler(socialService *SocialService) *SocialHandler {
+	return &SocialHandler{socialService: socialService}
+}
+
+func (sh *SocialHandler) Follow(c *gin.Context) {
+	var req FollowerRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	followerID, err := jwt.GetAccountID(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	social := &Social{
+		FollowerID: followerID,
+		VloggerID:  req.VloggerID,
+	}
+	if err := sh.socialService.Follow(c.Request.Context(), social); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"messagr": "follow success"})
+}
+func (sh *SocialHandler) Unfollow(c *gin.Context) {
+	var req UnFollowerRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	followerID, err := jwt.GetAccountID(c)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return
+	}
+	social := &Social{
+		FollowerID: followerID,
+		VloggerID:  req.VloggerID,
+	}
+	if err := sh.socialService.Unfollow(c.Request.Context(), social); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "unfollow success"})
+}
+func (sh *SocialHandler) GetAllFollowers(c *gin.Context) {
+	var req GetAllFollowersRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	vloggerID := req.VloggerID
+	if vloggerID == 0 {
+		accountID, err := jwt.GetAccountID(c)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			return
+		}
+		vloggerID = accountID
+	}
+	followers, err := sh.socialService.GetAllFollowers(c.Request.Context(), vloggerID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, GetAllFollowersResponse{Followers: followers})
+}
+
+func (sh *SocialHandler) GetAllVloggers(c *gin.Context) {
+	var req GetAllVloggersRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	followerID := req.FollowerID
+	if followerID == 0 {
+		accountID, err := jwt.GetAccountID(c)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			return
+		}
+		followerID = accountID
+	}
+	vloggers, err := sh.socialService.GetAllVloggers(c.Request.Context(), followerID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, GetAllVloggersResponse{Vloggers: vloggers})
+}
