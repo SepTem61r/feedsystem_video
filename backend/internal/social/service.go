0745e61a64a9ad04e4e1b0d3b363bf313c7d6fdb
SepTem61r <SepTem61r@example.com>
2026-05-10 19:34:59 +0800

    social service逻辑

--- backend/internal/social/service.go
+++ backend/internal/social/service.go
@@ -8,3 +8,80 @@ type SocialService struct {
 	socialRepository  *SocialRepository
 	accountRepository *account.AccountRepository
 }
+
+func NewSocialService(socialRepo *SocialRepository, accountRepo *account.AccountRepository, socialMQ *rabbitmq.SocialMQ) *SocialService {
+	return &SocialService{
+		socialRepo:  socialRepo,
+		accountRepo: accountRepo,
+		socialMQ:    socialMQ,
+	}
+}
+
+// Follow关注
+func (ss *SocialService) Follow(ctx context.Context, social *Social) error {
+	_, err := ss.accountRepo.FindByID(ctx, social.VloggerID)
+	if err != nil {
+		return err
+	}
+	_, err = ss.accountRepo.FindByID(ctx, social.FollowerID)
+	if err != nil {
+		return err
+	}
+	if social.FollowerID == social.VloggerID {
+		return errors.New("can not follow yourself")
+	}
+	isFollowed, err := ss.socialRepo.IsFollowed(ctx, social)
+	if err != nil {
+		return nil
+	}
+	if isFollowed {
+		return errors.New("already follow")
+	}
+	if ss.socialMQ != nil {
+		if err := ss.socialMQ.Follow(ctx, social.FollowerID, social.VloggerID); err != nil {
+			return err
+		}
+	}
+	return ss.socialRepo.Follow(ctx, social)
+}
+
+// Unfollow取关
+func (ss *SocialService) Unfollow(ctx context.Context, social *Social) error {
+	_, err := ss.accountRepo.FindByID(ctx, social.VloggerID)
+	if err != nil {
+		return err
+	}
+	_, err = ss.accountRepo.FindByID(ctx, social.FollowerID)
+	if err != nil {
+		return err
+	}
+	if social.FollowerID == social.VloggerID {
+		return errors.New("can not unfollow yourself")
+	}
+	isFollowed, err := ss.socialRepo.IsFollowed(ctx, social)
+	if err != nil {
+		return nil
+	}
+	if !isFollowed {
+		return errors.New("already unfollow")
+	}
+	return ss.socialRepo.Unfollow(ctx, social)
+}
+
+// GetAllFollowers得到所有粉丝
+func (ss *SocialService) GetAllFollowers(ctx context.Context, vloggerID uint) ([]*account.Account, error) {
+	_, err := ss.accountRepo.FindByID(ctx, vloggerID)
+	if err != nil {
+		return nil, err
+	}
+	return ss.socialRepo.GetAllFollowers(ctx, vloggerID)
+}
+
+// GetAllVloggers得到所有关注
+func (ss *SocialService) GetAllVloggers(ctx context.Context, followerID uint) ([]*account.Account, error) {
+	_, err := ss.accountRepo.FindByID(ctx, followerID)
+	if err != nil {
+		return nil, err
+	}
+	return ss.socialRepo.GetAllVloggers(ctx, followerID)
+}
